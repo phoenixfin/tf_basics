91673c90c373a9f7afe75058a3d1660743acce54
Aditya Firman Ihsan <aditya.fphoenix@gmail.com>
2020-04-04 09:20:23 +0700

    wrote basic csv reader

---
+++ support.py
@@ -0,0 +1,31 @@
+"""
+support functions
+"""
+
+import pandas as pd
+import numpy as np
+import os
+
+def get_datasets_list():
+    return os.listdir('datasets')
+
+def get_csv_list(data_dir):
+    return os.listdir('datasets/'+data_dir)
+
+def read_data(data_dir, csv_file):
+    path = 'datasets/' + data_dir + '/' + csv_file
+    print(path)
+    df = pd.read_csv(path, sep=",")
+    df = df.reindex(
+        np.random.permutation(df.index))
+    print("Overview:")
+    print(df.head(),'\n')
+    print('Basic statistical values:\n', df.describe())
+    return df
+
+if __name__ == "__main__":
+    # example
+    print(get_datasets_list())
+    print(get_csv_list('data-gempa'))
+
+    read_data('data-gempa', '2009.csv')
