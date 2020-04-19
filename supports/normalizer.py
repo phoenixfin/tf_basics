006f711145f77d278ef8c8e785559169b80b5fea
Aditya Firman Ihsan <aditya.fphoenix@gmail.com>
2020-04-19 11:08:48 +0700

    add some flow management in transforming

--- supports/normalizer.py
+++ supports/normalizer.py
@@ -3,9 +3,12 @@ import numpy as np
 
 class DataNormalizer(object):
     def __init__(self, dataframe, feature=None):
+        self.old_df = dataframe
+        self.ori_df = dataframe
         self.df = dataframe
         if feature == None: feature = self.df.columns[0]
         self.feature = feature
+        self.transform_list = []
 
     @property
     def data(self):
@@ -14,6 +17,10 @@ class DataNormalizer(object):
     @data.setter
     def data(self, value):
         self.df[self.feature] = value
+
+    def _general_transform(self, method_name):
+        self.transform_list.append(method_name+'_'+self.feature)
+        self.old_df = self.df
 
     def change_feature(self, new_feature):
         self.feature = new_feature
@@ -26,7 +33,15 @@ class DataNormalizer(object):
         dmin = float(self.data.min())
         return dmax, dmin, dmax-dmin
 
+    def undo_previous_transformation(self):
+        self.df = self.old_df
+        self.transform_list.pop()
+
+    def restore_dataframe(self):
+        self.df = self.ori_df
+
     def binning(self, method, bins=10, labels=[]):
+        self._general_transform('binning')
         if method == 'quantile':
             ds = 1./bins
             grids = [self.data.quantile(ds*i) for i in range(bins+1)]
@@ -35,15 +50,18 @@ class DataNormalizer(object):
             ds = drange/bins
             grids = np.arange(dmin, dmax+ds, ds)
         if not labels: labels = [method+'_bin_'+str(j) for j in range(bins)]
-        return pd.cut(self.data, bins=grids, labels=labels)
+        self.df[feature+' bin'] = pd.cut(self.data, bins=grids, labels=labels)
 
     def bucketing(self, vocab, labels, group=None):
+        self._general_transform('bucketing')
         condition = [[k in vocab[label] for k in self.data] for label in labels]
         if group is None:
             group = self.feature + ' grouping'
+        self.df.pop(self.feature)            
         self.df[group] = np.select(condition, labels, default='Other')
     
     def one_hot_encoding(self):
+        self._general_transform('one_hot_encoding')        
         categorized = pd.get_dummies(self.data)
         self.df.pop(self.feature)
         self.df = pd.concat((self.df, categorized), axis=1)        
@@ -60,6 +78,7 @@ class DataNormalizer(object):
         return upper_lim, lower_lim, outliers
 
     def data_filler(self, method, fillzero = False):
+        self._general_transform('data_filling')        
         fill_value = {
             'zero' : 0,
             'median' : self.data.median(),
@@ -73,17 +92,21 @@ class DataNormalizer(object):
         self.data = getattr(self, '_'+method_name)(*args)
         
     def _range_scaling(self, lower=0, upper=1):
+        self._general_transform('range_scaling')        
         dmax, dmin, drange = self.get_range()
         scaled_range = upper-lower
         return lower + ((self.data-dmin)/old_range)*scaled_range        
 
     def _clipping(self, lower=None, upper=None):
+        self._general_transform('clipping')        
         return self.data.clip(lower, upper)
         
     def _log_scaling(self, base = np.e):
+        self._general_transform('log_scaling')        
         return np.log(self.data+1)/np.log(base)
     
     def _z_value_scaling(self):
+        self._general_transform('z_value_scaling')        
         mean, std = self.get_mean_std()
         return (self.data - mean)/(std+0.000001)
     
